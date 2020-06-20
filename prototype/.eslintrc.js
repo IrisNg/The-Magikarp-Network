ee5a2e9b0ceeab43c25e2109eac029669513d2c8
Iris Ng <irisngwl@gmail.com>
2020-06-20 22:36:18 +0800

    Setup working calendar algorithm

--- prototype/.eslintrc.js
+++ prototype/.eslintrc.js
@@ -1,11 +1,21 @@
 module.exports = {
-    extends: [
-        // add more generic rulesets here, such as:
-        // 'eslint:recommended',
-        'plugin:vue/vue3-recommended'
+  extends: [
+    // add more generic rulesets here, such as:
+    // 'eslint:recommended',
+    'plugin:vue/vue3-recommended',
+  ],
+  rules: {
+    // override/add rules settings here, such as:
+    // 'vue/no-unused-vars': 'error'
+    'vue/max-attributes-per-line': [
+      2,
+      {
+        singleline: 20,
+        multiline: {
+          max: 1,
+          allowFirstLine: false,
+        },
+      },
     ],
-    rules: {
-        // override/add rules settings here, such as:
-        // 'vue/no-unused-vars': 'error'
-    }
-}
+  },
+};
--- prototype/src/components/Scheduler.vue
+++ prototype/src/components/Scheduler.vue
@@ -1,12 +1,83 @@
+/* eslint-disable vue/max-attributes-per-line */ /* eslint-disable vue/max-attributes-per-line */
 <template>
   <div class="scheduler">
     <p>Scheduler</p>
+    <table>
+      <thead>
+        <th v-for="dayName in daysOfWeek" :key="dayName">
+          {{ dayName }}
+        </th>
+      </thead>
+      <tbody>
+        <tr v-for="(week, weekIndex) in calendar" :key="weekIndex">
+          <td v-for="day in week" :key="day">
+            {{ day }}
+          </td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script>
-export default {};
+  import dayjs from 'dayjs';
+
+  export default {
+    data() {
+      return {
+        calendar: [],
+        daysOfWeek: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+      };
+    },
+    created() {
+      let daysList = [];
+      let daysInMonth = dayjs().daysInMonth();
+
+      for (var i = 1; i <= daysInMonth; i++) {
+        daysList.push(dayjs().date(i));
+      }
+
+      // console.log('daysList', daysList);
+      // console.log('testing', daysList[0].get('day'));
+      let startingDayOfWeek = daysList[0].get('day'); //sunday = 0, Saturday = 6
+
+      // startingDayOfWeek= 3;
+      let calendarList = [];
+      for (var i = 0; i < startingDayOfWeek; i++) {
+        calendarList.push('');
+      }
+
+      calendarList = [...calendarList, ...daysList];
+
+      // console.log('calendarList', calendarList);
+      let final = [];
+      let holder = [];
+      calendarList.forEach((day, index) => {
+        holder.push(day);
+        if ((index + 1) % 7 === 0) {
+          final.push([...holder]);
+          holder = [];
+        }
+      });
+
+      final.push([...holder]);
+
+      console.log('final', final);
+
+      this.calendar = final.map((week) => {
+        let formattedWeek = week.map((day) => {
+          if (day) {
+            return day.get('date');
+          } else {
+            return day;
+          }
+        });
+        return formattedWeek;
+      });
+
+      console.log('data days', this.calendar);
+    },
+  };
 </script>
 
-<style lang="scss" scoped>
-</style>
+<style lang="scss" scoped></style>
