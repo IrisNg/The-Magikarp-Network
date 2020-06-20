18f5fa5dfd50ee8546b6d462ec989314cdf48242
Iris Ng <irisngwl@gmail.com>
2020-06-20 23:33:21 +0800

    Refactor calendar derivative logic

--- prototype/src/components/Scheduler.vue
+++ prototype/src/components/Scheduler.vue
@@ -9,9 +9,9 @@
         </th>
       </thead>
       <tbody>
-        <tr v-for="(week, weekIndex) in calendar" :key="weekIndex">
-          <td v-for="day in week" :key="day">
-            {{ day }}
+        <tr v-for="(week, weekIndex) in calendarTable" :key="weekIndex">
+          <td v-for="date in week" :key="date.id">
+            {{ date.date || '' }}
           </td>
         </tr>
       </tbody>
@@ -22,60 +22,67 @@
 <script>
   import dayjs from 'dayjs';
 
+  function getRenderObj(dateObj) {
+    return dateObj ? { id: dateObj.format('DD/MM/YYYY'), date: dateObj.get('date') } : '';
+  }
+
+  function getCalendarTable(dayjsMonthObj) {
+    let datesList = [],
+      numDaysInMonth = dayjsMonthObj.daysInMonth(); //eg 31
+
+    //Generate dayjs obj for each day in the month
+    for (let i = 1; i <= numDaysInMonth; i++) {
+      datesList.push(dayjsMonthObj.date(i));
+    }
+
+    let startingDayOfWeek = datesList[0].get('day'); //sunday = 0, Saturday = 6
+    // startingDayOfWeek= 3;
+
+    //Fill beginning until 1st day of the month is on the correct day of week
+    for (let i = 0; i < startingDayOfWeek; i++) {
+      datesList.unshift('');
+    }
+
+    console.log('datesList', datesList);
+
+    //Format list into table format [[dayjsObjs in week 1], [week 2], ...]
+    let { table } = datesList.reduce(
+      (acc, dateObj, index) => {
+        acc.holder.push(getRenderObj(dateObj));
+
+        let isLastDayOfWeek = (index + 1) % 7 === 0,
+          isLastItem = index === datesList.length - 1;
+
+        if (isLastDayOfWeek || isLastItem) {
+          acc.table.push([...acc.holder]);
+          acc.holder = [];
+        }
+
+        return acc;
+      },
+      { table: [], holder: [] }
+    );
+
+    return table;
+  }
+
   export default {
     data() {
       return {
-        calendar: [],
+        calendarTable: [],
         daysOfWeek: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
       };
     },
     created() {
-      let daysList = [];
-      let daysInMonth = dayjs().daysInMonth();
-
-      for (var i = 1; i <= daysInMonth; i++) {
-        daysList.push(dayjs().date(i));
+      let remainingMonths = [];
+      let currentMonthIndex = dayjs().get('month')
+      for (let i=currentMonthIndex; i < 12; i++) {
+        remainingMonths.push(i)
       }
+      console.log('remain', remainingMonths)
+      this.calendarTable = getCalendarTable(dayjs());
 
-      // console.log('daysList', daysList);
-      // console.log('testing', daysList[0].get('day'));
-      let startingDayOfWeek = daysList[0].get('day'); //sunday = 0, Saturday = 6
-
-      // startingDayOfWeek= 3;
-      let calendarList = [];
-      for (var i = 0; i < startingDayOfWeek; i++) {
-        calendarList.push('');
-      }
-
-      calendarList = [...calendarList, ...daysList];
-
-      // console.log('calendarList', calendarList);
-      let final = [];
-      let holder = [];
-      calendarList.forEach((day, index) => {
-        holder.push(day);
-        if ((index + 1) % 7 === 0) {
-          final.push([...holder]);
-          holder = [];
-        }
-      });
-
-      final.push([...holder]);
-
-      console.log('final', final);
-
-      this.calendar = final.map((week) => {
-        let formattedWeek = week.map((day) => {
-          if (day) {
-            return day.get('date');
-          } else {
-            return day;
-          }
-        });
-        return formattedWeek;
-      });
-
-      console.log('data days', this.calendar);
+      console.log('injected calendarTable', this.calendarTable);
     },
   };
 </script>
